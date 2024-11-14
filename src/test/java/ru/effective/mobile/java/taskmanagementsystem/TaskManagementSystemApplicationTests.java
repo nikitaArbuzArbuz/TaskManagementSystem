fc29c2751655194603fcdd7200a6832d227a55e0
Nikita <talker_64@vk.com>
2024-11-14 22:15:49 +0300

    init commit

---
+++ src/test/java/ru/effective/mobile/java/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.effective.mobile.java.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
